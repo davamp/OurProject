64d17325b68048680db50aa5c17d005b5d3ce44d
da <da@das-macbook-pro.local>
2016-12-12 19:07:59 +0700

    Initial Commit

---
+++ HotNewsOnline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotNewsOnline
+//
+//  Created by da on 12/12/2559 BE.
+//  Copyright © 2559 BE da. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
